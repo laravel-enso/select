7d71b81840d5fd6e6f3be959d1a90ea20201bb8c
mishu <mishu@xtelecom.ro>
2017-05-20 14:47:08 +0300

    added support for options list prop changing

--- src/resources/assets/js/components/VueSelect.vue
+++ src/resources/assets/js/components/VueSelect.vue
@@ -24,7 +24,6 @@
     export default {
 
         props: {
-
             name: {
                 type: String,
                 default: null
@@ -57,105 +56,105 @@
                 default: null
             }
         },
-        computed: {
-            isServerSide: function () {
 
+        computed: {
+            isServerSide() {
                 return this.options ? false : true;
             }
         },
+
         watch: {
-
-            /* should only be used with server side vue select functionality */
             customParams: {
-
                 handler: 'getOptionsList',
                 deep: true
             },
             selected: {
-
                 handler: 'handleSelectedChange'
+            },
+            options: {
+                handler: 'handleOptionsChange',
+                deep: true
             }
         },
-        data: function () {
 
+        data() {
             return {
                 optionsList: this.options || [],
                 selectedOptions: this.selected ? this.selected : (this.multiple ? [] : this.selected)
             };
         },
+
         methods: {
-
-            getOptionsList: function () {
-
-                var query = $('#bs-select-' + this._uid + ' input').val() || '', //we don't want undefined
+            getOptionsList() {
+                let query = $('#bs-select-' + this._uid + ' input').val() || '', //we don't want undefined
                     params = {
                         customParams: this.customParams,
                         query: query,
                         selected: this.selectedOptions
                     };
 
-                axios.get(this.source, {params: params}).then((response) => {
-
+                axios.get(this.source, {params: params}).then(response => {
                     this.optionsList = response.data;
 
                     if (this.multiple && this.optionsList.length === 0) {
-
                         this.optionsList = [ { key: null, value: ''} ];
                     }
                 }).then(() => {
-
                         $('#select-' + this._uid).selectpicker('refresh');
                 });
             },
-            removeSelection: function () {
-
+            removeSelection() {
                 this.selectedOptions = '';
-
                 //we need next tick for the race condition when selectpicker
                 //runs before vue finishes updating the DOM
-                this.$nextTick(function () {
-
+                this.$nextTick(() => {
                     $('#select-' + this._uid).selectpicker('refresh');
                     this.$emit('input', this.selectedOptions);
                 });
             },
-            handleSelectedChange: function() {
-
+            handleSelectedChange() {
                 this.selectedOptions = this.selected;
 
                 if (this.isServerSide) {
-
-                    this.getOptionsList();
-                } else {
-
-                    $('#select-' + this._uid).selectpicker('val', this.selectedOptions);
-                    this.$emit('input', this.selectedOptions);
+                    return this.getOptionsList();
                 }
+
+                $('#select-' + this._uid).selectpicker('val', this.selectedOptions);
+                this.$emit('input', this.selectedOptions);
+            },
+            handleOptionsChange() {
+                this.optionsList = this.options;
+                this.removeSelection();
+            },
+            init() {
+                $('#select-' + this._uid).selectpicker({
+                    width: '100%',
+                    liveSearch: true,
+                    size: 5,
+                    actionsBox: true,
+                    title: $.fn.selectpicker.defaults.noneSelectedText
+                });
             }
         },
-        mounted: function () {
 
-            $('#select-' + this._uid).selectpicker({
-                width: '100%',
-                liveSearch: true,
-                size: 5,
-                actionsBox: true,
-                title: $.fn.selectpicker.defaults.noneSelectedText
-            });
+        mounted() {
+            this.init();
 
             if (this.selectedOptions) {
-
                 this.$emit('input', this.selectedOptions);
                 //necesary for using without server-side
                 $('#select-' + this._uid).selectpicker('val', this.selectedOptions);
             }
 
             if (this.isServerSide) {
-
                 this.getOptionsList();
-                var self = this;
+                let self = this;
                 $('#bs-select-' + this._uid + ' input').on('input', _.throttle(self.getOptionsList, 200));
             }
+        },
+
+        beforeDestroy() {
+            $('#select-' + this._uid).selectpicker('destroy');
         }
     }
 
