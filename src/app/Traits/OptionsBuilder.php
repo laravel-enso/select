059758e91c267127f8a94e39e26f35b513130118
mishu <mishu@xtelecom.ro>
2019-01-19 11:24:37 +0200

    adds the request object to query()

--- src/app/Traits/OptionsBuilder.php
+++ src/app/Traits/OptionsBuilder.php
@@ -2,15 +2,16 @@
 
 namespace LaravelEnso\Select\app\Traits;
 
+use Illuminate\Http\Request;
 use LaravelEnso\Select\app\Classes\OptionsBuilder as Builder;
 
 trait OptionsBuilder
 {
-    public function options()
+    public function options(Request $request)
     {
         return new Builder(
             method_exists($this, 'query')
-                ? $this->query()
+                ? $this->query($request)
                 : $this->model::query(),
             $this->trackBy ?? 'id',
             $this->queryAttributes ?? ['name']
