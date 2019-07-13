a65cc61447feb05a3d19790262f5232f1a3f351a
reza <raftx24@gmail.com>
2019-07-13 23:27:31 +0430

    make OptionTest independent from enso framework

--- tests/features/OptionTest.php
+++ tests/features/OptionTest.php
@@ -1,17 +1,21 @@
 <?php
+
 use Tests\TestCase;
 use Illuminate\Http\Request;
-use LaravelEnso\Core\app\Models\User;
 use LaravelEnso\Select\app\Traits\OptionsBuilder;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Faker\Factory;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 
 class OptionTest extends TestCase
 {
-    use OptionsBuilder, RefreshDatabase;
+    use OptionsBuilder;
 
     private $testModel;
 
-    protected $queryAttributes = ['email', 'person.name'];
+    private $faker;
+
+    protected $queryAttributes = ['email', 'child.name'];
 
     public function setUp(): void
     {
@@ -19,11 +23,15 @@ class OptionTest extends TestCase
 
         $this->withoutExceptionHandling();
 
-        $this->seed()
-            ->actingAs(User::first());
+        $this->faker = Factory::create();
 
-        $this->testModel = factory(User::class)
-            ->create();
+        $this->createTables();
+
+        $this->testModel = $this->createParentModels();
+
+        $this->createChildModel($this->testModel);
+
+        $this->createChildModel($this->createParentModels());
     }
 
     /** @test */
@@ -31,7 +39,7 @@ class OptionTest extends TestCase
     {
         $response = $this->requestResponse();
 
-        $this->assertCount(User::count(), $response);
+        $this->assertCount(ParentModel::count(), $response);
 
         $this->assertTrue($this->whithinResponse($response));
     }
@@ -43,11 +51,11 @@ class OptionTest extends TestCase
 
         $this->assertCount(0, $response);
     }
-    
+
     /** @test */
     public function can_get_empty_options_with_restricting_query()
     {
-        $response = $this->requestResponse(['query' => 'NO_NAME']);
+        $response = $this->requestResponse(['query' => 'NO_VALUE']);
 
         $this->assertCount(0, $response);
     }
@@ -57,7 +65,7 @@ class OptionTest extends TestCase
     {
         $response = $this->requestResponse([
             'value' => $this->testModel->id,
-            'query' => 'NO_NAME',
+            'query' => 'NO_VALUE',
         ]);
 
         $this->assertCount(1, $response);
@@ -79,7 +87,7 @@ class OptionTest extends TestCase
     public function can_get_filtered_on_nested_attrs_options()
     {
         $response = $this->requestResponse([
-            'query' => $this->testModel->person->name,
+            'query' => $this->testModel->child->name,
         ]);
 
         $this->assertTrue($this->whithinResponse($response));
@@ -89,7 +97,7 @@ class OptionTest extends TestCase
     public function can_get_options_with_pivot_params()
     {
         $response = $this->requestResponse([
-            'pivotParams' => ['role' => ['name' => $this->testModel->role->name]]
+            'pivotParams' => ['child' => ['name' => $this->testModel->child->name]]
         ]);
 
         $this->assertTrue($this->whithinResponse($response));
@@ -115,7 +123,7 @@ class OptionTest extends TestCase
     {
         $request = new Request();
 
-        collect($params)->each(function($value, $key) use ($request) {
+        collect($params)->each(function ($value, $key) use ($request) {
             $request->merge([
                 $key => is_array($value) ? json_encode($value) : $value
             ]);
@@ -126,8 +134,56 @@ class OptionTest extends TestCase
         );
     }
 
-    public function query(Request $request)
+    private function query(Request $request)
     {
-        return User::query();
+        return ParentModel::query();
     }
+
+
+    private function createParentModels()
+    {
+        return ParentModel::create([
+            'email' => $this->faker->email,
+        ]);
+    }
+
+    private function createChildModel($parent = null)
+    {
+        return ChildModel::create([
+            'name' => $this->faker->name,
+            'parent_id' => $parent !== null ? $parent->id : null,
+        ]);
+    }
+
+    private function createTables()
+    {
+        Schema::create('parent_models', function ($table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->timestamps();
+        });
+        
+        Schema::create('child_models', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('parent_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+}
+
+class ParentModel extends Model
+{
+    protected $fillable = ['email'];
+
+    public function child()
+    {
+        return $this->hasOne(ChildModel::class, 'parent_id');
+    }
+}
+
+class ChildModel extends Model
+{
+    protected $fillable = ['name', 'parent_id'];
 }
