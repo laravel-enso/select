385f3936b9096b908b63b062cd89e4d674c4fa28
Mihai <mihai.ocneanu@gmail.com>
2019-10-17 11:00:37 +0300

    refactor typeahead trait

--- src/app/Traits/TypeaheadBuilder.php
+++ src/app/Traits/TypeaheadBuilder.php
@@ -8,31 +8,32 @@ use LaravelEnso\Select\app\Services\Options;
 
 trait TypeaheadBuilder
 {
+
+    protected $request;
+
     public function __invoke(Request $request)
     {
-        $payload = $this->convert($request);
+        $this->request = $this->convertRequest($request);
 
         return (new Options(
             method_exists($this, 'query')
-                ? $this->query($payload)
+                ? $this->query($this->request)
                 : $this->model::query(),
             $request->get('trackBy') ?? config('enso.select.trackBy'),
             $this->queryAttributes ?? config('enso.select.queryAttributes'),
             $this->resource ?? null
-        ))->toResponse($payload);
+        ))->toResponse($this->request);
     }
 
-    private function convert($request)
+    private function convertRequest(Request $request)
     {
-        return (new Obj())
-            ->set('customParams', $this->reEncode('custom', $request->get('params')))
-            ->set('pivotParams', $this->reEncode('pivot', $request->get('params')))
-            ->set('query', $request->get('query'))
-            ->set('paginate', $request->get('paginate'));
-    }
+        $params = json_decode($request->get('params'));
 
-    private function reEncode(string $name, string $json)
-    {
-        return json_encode(json_decode($json)->$name);
+        return new Obj([
+            'customParams' => json_encode($params->custom),
+            'pivotParams' => json_encode($params->pivot),
+            'query' => $request->get('query'),
+            'paginate' => $request->get('paginate'),
+        ]);
     }
 }
